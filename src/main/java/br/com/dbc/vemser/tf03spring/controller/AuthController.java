5801777f1d195a1c03bb504dd3d81218721d1ad2
RenanBilhan <renan.bilhan@hotmail.com>
2023-08-18 14:05:11 -0300

    Feat jwt token

--- src/main/java/br/com/dbc/vemser/tf03spring/controller/AuthController.java
+++ src/main/java/br/com/dbc/vemser/tf03spring/controller/AuthController.java
@@ -1,11 +1,15 @@
 package br.com.dbc.vemser.tf03spring.controller;
 
+
 import br.com.dbc.vemser.tf03spring.dto.LoginDTO;
 import br.com.dbc.vemser.tf03spring.exception.RegraDeNegocioException;
 import br.com.dbc.vemser.tf03spring.model.UsuarioEntity;
 import br.com.dbc.vemser.tf03spring.security.TokenService;
 import br.com.dbc.vemser.tf03spring.service.UsuarioService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/auth")
@@ -22,14 +25,27 @@ import java.util.Optional;
 public class AuthController {
     private final UsuarioService usuarioService;
     private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
 
     @PostMapping
     public String auth(@RequestBody @Valid LoginDTO loginDTO) throws RegraDeNegocioException {
-        Optional<UsuarioEntity> byLoginAndSenha = usuarioService.findByLoginAndSenha(loginDTO.getLogin(), loginDTO.getSenha());
-        if (byLoginAndSenha.isPresent()) {
-            return tokenService.getToken(byLoginAndSenha.get());
-        } else {
-            throw new RegraDeNegocioException("usuário e senha inválidos");
-        }
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                        loginDTO.getLogin(),
+                        loginDTO.getSenha()
+                );
+        Authentication authentication =
+                authenticationManager.authenticate(
+                        usernamePasswordAuthenticationToken);
+        UsuarioEntity usuarioValidado = (UsuarioEntity) authentication.getPrincipal();
+        return tokenService.generateToken(usuarioValidado);
+
     }
+
 }
+//        Optional<UsuarioEntity> byLoginAndSenha = usuarioService.findByLoginAndSenha(loginDTO.getLogin(), loginDTO.getSenha());
+//        if (byLoginAndSenha.isPresent()) {
+//            return tokenService.getToken(byLoginAndSenha.get());
+//        } else {
+//            throw new RegraDeNegocioException("usuário e senha inválidos");
+//        }
